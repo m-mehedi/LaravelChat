7e998e28abd58946d008aea7a8bc495ec94a4086
MEHEDI <me.mehedi@hotmail.com>
2022-03-17 10:32:56 +0600

    Models Relationship

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Message;
+use App\Models\User;
 
 class Conversation extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'sender_id',
+        'receiver_id',
+        'last_time_message'
+    ];
+
+    // relationship
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Conversation;
+use App\Models\User;
 
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'sender_id',
+        'receiver_id',
+        'conversation_id',
+        'read',
+        'type',
+        'body'
+    ];
+
+    // relationship
+
+    public function conversation(){
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class,'sender_id');
+    }
 }
