cccae94864a7b726899972cbe039935b9322d3f5
MEHEDI <me.mehedi@hotmail.com>
2022-03-19 23:38:27 +0600

    Chat List Design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,9 +1,38 @@
 <div>
     <div class="chatlist_header">
 
+        <div class="title">
+            Chat
+        </div>
+
+        <div class="img_container">
+            <img src="https://picsum.photos/id/237/200/300" alt="">
+        </div>
+
     </div>
 
     <div class="chatlist_body">
+
+        <div class="chatlist_item">
+            <div class="chatlist_img_container">                
+            <img src="https://picsum.photos/id/200/200/300" alt="">
+            </div>
+
+            <div class="chatlist_info">
+                <div class="top_row">
+                    <div class="list_username">Mehedi</div>
+                    <span class="date">2d</span>                    
+                </div>
+                <div class="bottom_row">
+                    <div class="message_body truncate">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veritatis aliquam dolores accusantium nobis id veniam ex. Esse magnam consectetur deserunt sequi, tenetur magni, sapiente illo obcaecati doloribus eaque ab.
+                    </div>
+                    <div class="unread_count">
+                        56
+                    </div>                  
+                </div>
+            </div>
+        </div>
         
     </div>
 </div>
