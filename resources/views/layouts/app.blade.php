afdbb8b90473d57044e01b82211421b78f107a1a
MEHEDI <me.mehedi@hotmail.com>
2022-03-20 00:44:32 +0600

    Chatbox Header Design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,16 +9,19 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/chat.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+<!-- Styles -->
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/chat.css') }}">
 
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/js/all.min.js" integrity="sha512-Cvxz1E4gCrYKQfz6Ne+VoDiiLrbFBvc6BPh4iyKo2/ICdIodfgc5Q9cBjRivfQNUXBCEnQWcEInSXsvlNHY/ZQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -1,6 +1,25 @@
 <div>
    <div class="chatbox_header">
+      <div class="return">
+         <i class="fa-solid fa-arrow-left"></i>
+      </div>
+      <div class="img_container">
 
+         <img src="https://picsum.photos/id/231/200/300" alt="">
+      </div>
+      <div class="name">Mehedi</div>
+
+      <div class="info">
+         <div class="info_item">
+            <i class="fa-solid fa-phone"></i>
+         </div>
+         <div class="info_item">
+            <i class="fa-solid fa-image"></i>
+         </div>
+         <div class="info_item">
+            <i class="fa-solid fa-circle-info"></i>
+         </div>
+      </div>
    </div>
 
    <div class="chatbox_body">
